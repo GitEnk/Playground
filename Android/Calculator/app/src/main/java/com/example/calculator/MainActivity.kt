445a5d4d07199e9f9406c5f2a480e5f58db0dab8
Erik <erik.carnebrink@live.com>
2021-08-13 13:53:34 +0200

    Calculator done!

--- Android/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Android/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,115 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun onDigit(view: View) {
+        var tvInput = findViewById<TextView>(R.id.tvInput)
+        tvInput.append((view as Button).text)
+        lastNumeric = true
+
+    }
+
+    fun onClear(view: View){
+        //var tvInput = findViewById<TextView>(R.id.tvInput)
+        findViewById<TextView>(R.id.tvInput).text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot){
+            findViewById<TextView>(R.id.tvInput).append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onEqual(view: View){
+        if(lastNumeric){
+            var tvValue = findViewById<TextView>(R.id.tvInput).text.toString()
+            var prefix = ""
+            try{
+                if(tvValue.startsWith("-")){
+                        prefix = "-"
+                        tvValue = tvValue.substring(1)
+                }
+
+                if(tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()){
+                        one = prefix+one
+                    }
+                    findViewById<TextView>(R.id.tvInput).text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                } else if(tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()){
+                        one = prefix+one
+                    }
+                    findViewById<TextView>(R.id.tvInput).text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                } else if(tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()){
+                        one = prefix+one
+                    }
+                    findViewById<TextView>(R.id.tvInput).text = removeZeroAfterDot((one.toDouble()/two.toDouble()).toString())
+                } else if(tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()){
+                        one = prefix+one
+                    }
+                    findViewById<TextView>(R.id.tvInput).text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                }
+            } catch (e: Exception){}
+        }
+    }
+    fun onOperator(view: View){
+        var tvInput = findViewById<TextView>(R.id.tvInput)
+        if(lastNumeric && !isOperatorAdded(tvInput.text.toString())){
+            tvInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+        }
+    }
+
+    private fun removeZeroAfterDot(result: String) : String{
+        var value = result
+        if(result.contains(".0")){
+            value = result.substring(0, result.length-2)
+        }
+        return value
+    }
+
+
+    private fun isOperatorAdded(value: String) : Boolean{
+        return if(value.startsWith("-")){
+            false
+        } else {
+            value.contains("/") ||
+            value.contains("+") ||
+            value.contains("*") ||
+            value.contains("-")
+        }
     }
 }
